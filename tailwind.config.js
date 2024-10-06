463dfae6ff2375616f00df6863eb6fb1f4d3cec2
nhanbui1512 <buithiennhan0345@gmail.com>
2024-10-06 21:22:14 +0700

    config tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0c66e4',
+        primaryCyan: '#e9f2ff',
+        textColor: '#172b4d',
+        hoverBackground: '#091e420f',
+        defaultLayoutHeight: '60px',
+        defaultLayoutWidth: '1150px',
+        defaultLayoutHorizontalSpacer: '24px',
+      },
+    },
   },
   plugins: [],
 };
