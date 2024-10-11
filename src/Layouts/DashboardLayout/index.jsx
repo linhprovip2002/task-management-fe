0200dbc01d5458bded885e42324e1f05ff330b5f
Nhan Bui <buithiennhan0345@gmail.com>
2024-10-11 15:11:30 +0700

    create header settings page

--- src/Layouts/DashboardLayout/index.jsx
+++ src/Layouts/DashboardLayout/index.jsx
@@ -13,7 +13,6 @@ const DashBoardLayout = ({ children }) => {
   // const workspaceId = "workspaceId";
 
   const { workspaceInfo, isLoading } = useGetWorkspaceByUser();
-  console.log(workspaceInfo);
 
   const isActiveClassname = (path) => {
     return location.pathname === path ? 'bg-blue-100' : 'hover:bg-gray-200';
@@ -50,34 +49,32 @@ const DashBoardLayout = ({ children }) => {
             {/* Block for each worckspace */}
             {workspaceInfo.map((workspace) => {
               return (
-                <>
-                  <Collapse
-                    key={workspace.id}
-                    value={false}
-                    position="right"
-                    title={
-                      <div className="flex gap-4 items-center text-base">
-                        <Avatar sx={{ width: 28, height: 28 }}>{workspace.title[0]}</Avatar>
-                        <div className="text-xl font-bold">{workspace.title}</div>
-                      </div>
-                    }
-                  >
-                    <div className="flex flex-col gap-4 ml-4">
-                      {WorkSpaceItems(workspace.id).map((item, index) => {
-                        return (
-                          <div
-                            key={index}
-                            className={`hover:cursor-pointer pl-10 py-2 rounded-md ${isActiveClassname(item.path)}`}
-                            onClick={() => navigate(item.path)}
-                          >
-                            {item.icon}
-                            <span className="ml-2">{item.title}</span>
-                          </div>
-                        );
-                      })}
+                <Collapse
+                  key={workspace.id}
+                  value={false}
+                  position="right"
+                  title={
+                    <div className="flex gap-4 items-center text-base">
+                      <Avatar sx={{ width: 28, height: 28 }}>{workspace.title[0]}</Avatar>
+                      <div className="text-xl font-bold">{workspace.title}</div>
                     </div>
-                  </Collapse>
-                </>
+                  }
+                >
+                  <div className="flex flex-col gap-4 ml-4">
+                    {WorkSpaceItems(workspace.id).map((item, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className={`hover:cursor-pointer pl-10 py-2 rounded-md ${isActiveClassname(item.path)}`}
+                          onClick={() => navigate(item.path)}
+                        >
+                          {item.icon}
+                          <span className="ml-2">{item.title}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </Collapse>
               );
             })}
           </div>
--- src/Pages/Workspaces/Settings/index.jsx
+++ src/Pages/Workspaces/Settings/index.jsx
@@ -1,7 +1,49 @@
+import { Avatar, Button, Divider } from '@mui/material';
+import { deepOrange } from '@mui/material/colors';
+import LockIcon from '@mui/icons-material/Lock';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import EditIcon from '@mui/icons-material/Edit';
 const WorkspaceSettings = () => {
   return (
-    <div>
-      <h1>Workspace Setting</h1>
+    <div className="workSpaceSetting px-8">
+      {/* header  */}
+      <div className="flex justify-between pb-8">
+        <div>
+          <div className="flex gap-[10px] items-center text-[var(--text-color)]">
+            <Avatar sx={{ bgcolor: deepOrange[500], borderRadius: 1, width: 60, height: 60 }} variant="square">
+              N
+            </Avatar>
+            <div>
+              <div className="flex items-center">
+                <h2 className="text-xl font-semibold">BKDN</h2>
+                <button className="w-6 h-6 hover:bg-[var(--hover-background)] cursor-pointer flex items-center justify-center ml-2 rounded-md">
+                  <EditIcon sx={{ fontSize: '16px' }} />
+                </button>
+              </div>
+              <span className="text-xs flex  text-[#44546F]">
+                <LockIcon sx={{ fontSize: 16 }} />
+                Private
+              </span>
+            </div>
+          </div>
+          <div className="text-xs text-[var(--text-color)]">project</div>
+        </div>
+        <div>
+          <Button
+            sx={{
+              textTransform: 'none',
+              paddingY: '6px',
+              paddingX: '12px',
+            }}
+            variant="contained"
+            startIcon={<PersonAddIcon fontSize="inherit" />}
+          >
+            Invite workspace members
+          </Button>
+        </div>
+      </div>
+      <Divider component={'div'} />
+      <div>Body</div>
     </div>
   );
 };
