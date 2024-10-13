7f728c52aef1f9d9f727dc681a8a7929bdf97206
nhanbui1512 <buithiennhan0345@gmail.com>
2024-10-13 22:27:19 +0700

    fix import file

--- src/Pages/Workspaces/DashBoard/index.jsx
+++ src/Pages/Workspaces/DashBoard/index.jsx
@@ -1,15 +1,14 @@
-import { Divider } from "@mui/material";
-import PersonIcon from "@mui/icons-material/Person";
-import { Board } from "../../../Components/Board/Board";
-import { CreateNewBoard } from "../../../Components/CreateNewBoard/CreateNewBoard";
-import { useEffect, useState } from "react";
-import { getBoard } from "../../../Services/API/ApiBoard/apiBoard";
-import { BoardInformation } from "../../../Components/BoardInformation/BoardInformation";
+import { Divider } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import { Board } from '../../../Components/Board/Board';
+import { CreateNewBoard } from '../../../Components/Modals/CreateNewBoard/CreateNewBoard';
+import { useEffect, useState } from 'react';
+import { getBoard } from '../../../Services/API/ApiBoard/apiBoard';
+import { BoardInformation } from '../../../Components/BoardInformation/BoardInformation';
 
 const DashBoard = () => {
   const [listBoard, setListBoard] = useState([]);
   const [open, setOpen] = useState(false);
-
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -20,15 +19,13 @@ const DashBoard = () => {
 
       setListBoard(data);
     } catch (error) {
-      console.error("Failed to fetch board data:", error);
+      console.error('Failed to fetch board data:', error);
     }
   };
 
   useEffect(() => {
     fetchBoardData();
   }, []);
-
-  
 
   return (
     <div>
@@ -44,13 +41,9 @@ const DashBoard = () => {
         {/* grid grid-cols-3 mt-4 gap-y-6 */}
         <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
           {listBoard.map((board, index) => {
-            return <Board key={index} board={board}/>;
+            return <Board key={index} board={board} />;
           })}
-          <CreateNewBoard
-           open={open}
-           handleOpen={handleOpen}
-           handleClose={handleClose}
-          />
+          <CreateNewBoard open={open} handleOpen={handleOpen} handleClose={handleClose} />
         </div>
       </div>
     </div>
