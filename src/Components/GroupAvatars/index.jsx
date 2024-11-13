7de31b2d83986917e5f5a88c1b535690e9d5d164
nhanbui1512 <buithiennhan0345@gmail.com>
2024-11-13 11:08:36 +0700

    Fix data board members

--- src/Components/GroupAvatars/index.jsx
+++ src/Components/GroupAvatars/index.jsx
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import { stringAvatar } from "../../Utils/color";
 
 function GroupAvatars({ users = [] }) {
+  users = users.filter((item) => item.user !== null);
   return (
     <AvatarGroup
       max={5}
