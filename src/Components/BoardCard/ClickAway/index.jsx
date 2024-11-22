09effc67fc9ce13a9a7505343e5fddfbfb06ea6c
phomai <thanhmai.20570@gmail.com>
2024-11-22 15:17:51 +0700

    remove css component clickaway

--- src/Components/BoardCard/ClickAway/index.jsx
+++ src/Components/BoardCard/ClickAway/index.jsx
@@ -3,7 +3,7 @@ import { memo } from "react";
 
 function ClickAway({ onClickAway, children }) {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-[200] bg-red-300 bg-opacity-50">
+    <div className="fixed inset-0 flex items-center justify-center z-[200]">
       <ClickAwayListener onClickAway={onClickAway}>{children}</ClickAwayListener>
     </div>
   );
