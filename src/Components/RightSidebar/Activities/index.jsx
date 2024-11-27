c859d55d830b7e83b2ae66eb172809040ab72fe6
Nhan Bui <buithiennhan0345@gmail.com>
2024-11-27 13:26:16 +0700

    paginate for activity

--- src/Components/RightSidebar/Activities/index.jsx
+++ src/Components/RightSidebar/Activities/index.jsx
@@ -3,25 +3,37 @@ import React, { useEffect, useState } from "react";
 import ActivityItem from "./ActivityItem";
 import { getActivities } from "../../../Services/API/ApiBoard/apiBoard";
 import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Activities() {
   const [isLoading, setIsLoading] = useState(true);
   const [activities, setActivities] = useState([]);
-
   const { idBoard } = useParams();
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(false);
 
-  useEffect(() => {
-    getActivities(idBoard)
+  const fetchData = () => {
+    setIsLoading(true);
+    getActivities({ boardId: idBoard, page: page, perPage: 20 })
       .then((res) => {
-        setActivities(res.docs);
+        setActivities((prevItems) => [...prevItems, ...res.docs]);
+        if (res.docs.length === 0) {
+          setHasMore(false);
+        }
+        setPage((prevPage) => prevPage + 1);
       })
       .catch((err) => {
-        console.error(err);
+        console.log(err);
       })
       .finally(() => {
         setIsLoading(false);
       });
-  }, [idBoard]);
+  };
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line
+  }, []);
   return (
     <Slide in={true} direction="left">
       <div className="flex flex-col">
@@ -31,9 +43,11 @@ export default function Activities() {
           </div>
         )}
         <div>
-          {activities.map((act, index) => (
-            <ActivityItem data={act} key={index} />
-          ))}
+          <InfiniteScroll dataLength={activities.length} next={fetchData} hasMore={hasMore}>
+            {activities.map((act, index) => (
+              <ActivityItem data={act} key={index} />
+            ))}
+          </InfiniteScroll>
         </div>
       </div>
     </Slide>
