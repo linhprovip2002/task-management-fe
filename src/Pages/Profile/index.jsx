713a1c05a58eddaa3d47296e5aeb2f52a0984543
nhanbui1512 <buithiennhan0345@gmail.com>
2024-11-30 21:26:35 +0700

    Fix avatar in profile page

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -1,39 +1,29 @@
 import { Avatar } from "@mui/material";
 import { useStorage } from "../../Contexts/Storage";
 import { ProfileTabs } from "./constants/ProfileTabs.constant";
-import { convertStringToColor } from "./helpers/convertStringToColor.js";
 import { Link, useLocation } from "react-router-dom";
 import Loading from "../../Components/Loading";
+import { stringAvatar } from "../../Utils/color.js";
 
 const tabItemStyles =
   "text-sm w-fit text-[var(--dark-slate-blue)] font-bold pt-2 pb-[9px] cursor-pointer hover:text-[var(--primary)] mr-4";
-
-function stringAvatar(name) {
-  return {
-    sx: {
-      bgcolor: convertStringToColor(name)
-    },
-    children: `${name.split(" ")?.[0]?.[0]}${name.split(" ")?.[1]?.[0]}`
-  };
-}
 
 export default function Profile() {
   const location = useLocation();
   const { userData } = useStorage();
 
   if (!userData) return <Loading />;
-
+  console.log(userData)
   return (
     <div>
       <div>
         <div className="max-w-[875px] pl-[48px] pr-8 py-[26px] flex">
           <div className="flex py-5">
             <div className="flex items-center justify-center w-12 h-12 mr-4 rounded-full">
-              {userData?.avatarUrl ? (
-                <Avatar alt={userData?.name} src={userData?.avatarUrl} />
-              ) : (
-                <Avatar {...stringAvatar(userData?.name || "")} />
-              )}
+            {/* <Avatar sx={{
+              textTransform: 'uppercase'
+            }}>{userData?.name[0]}</Avatar> */}
+            <Avatar {...stringAvatar(userData?.name)} alt={userData?.name} src={userData?.avatarUrl || ""} />
             </div>
             <div className="flex flex-col">
               <h2 className="text-[var(--text-color)] text-xl font-semibold">
